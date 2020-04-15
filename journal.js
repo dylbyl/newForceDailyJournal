4f21699d084bfb27e4d2801c68b6ed5b44773bc3
Dylan Bishop <mdylbishop@gmail.com>
2020-04-15 14:38:03 -0400

    finished practice #2

---
+++ journal.js
@@ -0,0 +1,16 @@
+// journalEntry Object, will be filled by the user-filled form in index.html
+const journalEntry = {
+    date : "4/13/2020",
+    conceptsCovered : "JavaScript and Objects",
+    entry : "We covered the beginning chapters of Book 2, and reviewed how to create and navigate Objects in JavaScript",
+    mood : "Okayy"
+}
+
+console.log(journalEntry)
+
+//array for storing journal entries
+var journalEntryArray = []
+
+
+//pushes the newest journal entry to the array
+journalEntryArray.push(journalEntry);
