690f36e34eab1c779ed600f9066dcb982558d0ba
Dylan Bishop <mdylbishop@gmail.com>
2020-04-17 09:42:12 -0400

    finished DOM additions

--- journal.js
+++ journal.js
@@ -9,8 +9,28 @@ const journalEntry = {
 console.log(journalEntry)
 
 //array for storing journal entries
-var journalEntryArray = []
-
+let journalEntryArray = [];
 
 //pushes the newest journal entry to the array
 journalEntryArray.push(journalEntry);
+
+//crates an HTML structure from a journalEntry Object
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return `
+    <h1>${journalEntry.conceptsCovered}</h1>
+    <div class="entry">${journalEntry.entry}</div>
+    <div class="mood">${journalEntry.mood}</div>
+    <div class="date">${journalEntry.date}</div>
+    `
+}
+
+
+const renderEntries = (journalEntryArray) =>{
+    let html = document.querySelector(".entry-log");
+    for (let i=0; i < journalEntryArray.length; i++){
+        html.innerHTML += makeJournalEntryComponent(journalEntryArray[i]);
+    }
+}
+
+renderEntries(journalEntryArray);
